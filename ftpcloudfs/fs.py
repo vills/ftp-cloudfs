vills/ftp-cloudfs

commit 7993548e67c38726ae0b57ec31ef948070503168

this check is not needed because we perform a head request to see if src is a manifest file

ftpcloudfs/fs.py
@@ -856,9 +856,8 @@ class ObjectStorageFS(object):
         if not self.isdir(posixpath.split(dst)[0]):
             raise IOSError(ENOENT, "Can't copy %r to %r, destination directory doesn't exist" % (src, dst))
 
-        # check src/dst containers
+        # check dst container
         self._container_exists(src_container_name)
-        self._container_exists(dst_container_name)
 
         # Do the rename of the file/dir
         headers = { 'x-copy-from': "/%s/%s" % (src_container_name, src_path) }

commit 4e46f98aeed86578c56bed60233eeab35e8bbb0f

Check the right container
Error introduced in: 7993548e67c38726ae0b57ec31ef948070503168

ftpcloudfs/fs.py
@@ -857,7 +857,7 @@ class ObjectStorageFS(object):
             raise IOSError(ENOENT, "Can't copy %r to %r, destination directory doesn't exist" % (src, dst))
 
         # check dst container
-        self._container_exists(src_container_name)
+        self._container_exists(dst_container_name)
 
         # Do the rename of the file/dir
         headers = { 'x-copy-from': "/%s/%s" % (src_container_name, src_path) }